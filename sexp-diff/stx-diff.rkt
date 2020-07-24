3a7d09d9197964ff624c4a435157e3b1651249da
William J. Bowman <wjb@williamjbowman.com>
2020-07-23 19:34:49 -0700

    More paren-shape tests; found a bug

--- sexp-diff/stx-diff.rkt
+++ sexp-diff/stx-diff.rkt
@@ -59,13 +59,33 @@
    '((define (f x) ((highlight:new -) (highlight:old +) (* x 2) (highlight:new 3) 1))))
 
   (require syntax/parse)
-  (check-equal?
-   (syntax-parse (stx-diff
-                  #'((1) 2 3 4)
-                  #'([1] 2 2 4))
-     [((head any ...))
-      (syntax-property #'head 'paren-shape)])
-   #\[)
+  (let ([diff (stx-diff
+               #'((1) 2 3 4)
+               #'([1] 2 2 4))])
+    (check-equal?
+     (syntax-parse diff
+       [((head any ...))
+        (syntax-property #'head 'paren-shape)])
+     #\[)
+
+    (check-false
+     (syntax-property diff 'paren-shape)))
+
+  (let ([diff
+         (stx-diff
+          #'(e integer? (let ([x e]) e))
+          #'(e (let ([x v]) v) (v integer?)))])
+
+    (check-false
+     (syntax-parse diff
+       [((_ _ _ (let bs _ ...) . _))
+        (syntax-property #'bs 'paren-shape)]))
+
+    (check-equal?
+     (syntax-parse diff
+       [((_ _ _ (let (b) _ ...) . _))
+        (syntax-property #'b 'paren-shape)])
+     #\[))
   )
 
 ;;; Code:
